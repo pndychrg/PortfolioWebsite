3fc2e3cfca3f6e67bd4f8fadf4ea490e2a89efaa
Chirag Pandey <pndy.chrg2003@gmail.com>
2023-12-15 23:54:16 +0530

    added hamburger icon in menu

---
+++ components/animated/MenuButton.jsx
@@ -0,0 +1,95 @@
+'use client'
+import { motion, Transition, SVGMotionProps } from 'framer-motion'
+
+const MenuButton = ({
+    isOpen = false,
+    width = 24,
+    height = 24,
+    strokeWidth = 2,
+    color = "#000",
+    transition = null,
+    lineProps = null,
+    ...props
+}) => {
+
+    const variant = isOpen ? "opened" : 'closed';
+
+    const top = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: 45,
+            translateY: 1.25
+        }
+    };
+    const center = {
+        closed: {
+            opacity: 1
+        },
+        opened: {
+            opacity: 0
+        }
+    };
+    const bottom = {
+        closed: {
+            rotate: 0,
+            translateY: 0
+        },
+        opened: {
+            rotate: -45,
+            translateY: -1.25,
+        }
+    };
+    lineProps = {
+        stroke: color,
+        strokeWidth: strokeWidth,
+        vectorEffect: "non-scaling-stroke",
+        initial: "closed",
+        animate: variant,
+        transition,
+        ...lineProps
+    };
+    const unitHeight = 6;
+    const unitWidth = (unitHeight * (width)) / (height);
+
+
+    return (
+        <motion.svg
+            viewBox={`0 0 ${unitWidth} ${unitHeight}`}
+            overflow="visible"
+            preserveAspectRatio="none"
+            width={width}
+            height={height}
+            {...props}
+        >
+            <motion.line
+                x1="0"
+                x2={unitWidth}
+                y1="0"
+                y2="0"
+                variants={top}
+                {...lineProps}
+            />
+            <motion.line
+                x1="0"
+                x2={unitWidth}
+                y1="2"
+                y2="2"
+                variants={center}
+                {...lineProps}
+            />
+            <motion.line
+                x1="0"
+                x2={unitWidth}
+                y1="4"
+                y2="4"
+                variants={bottom}
+                {...lineProps}
+            />
+        </motion.svg>
+    )
+}
+
+export default MenuButton
